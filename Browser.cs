2cc5063a4dcb19c345345e19c78c4efcb678f3f7
Datorium <hello@datorium.lv>
2021-02-20 14:44:28 +0200

    Homework added

--- Browser.cs
+++ Browser.cs
@@ -40,5 +40,12 @@ namespace ChromiumBrowserWinForms
             string url = AddressBar.Text;
             chromeBrowser.Load(url);
         }
+
+        //Homework for Feb 27, 2021
+        //1. Add Back and Forward buttons, make sure that they work
+        //2. Add Reload button
+        //3. Check, if adress does not start with http/www, then request a Google search
+        //4. Make sure that when you press enter in the Address Bar, the browser vists your URL
+        //5. Your own feature
     }
 }
